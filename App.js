41c8c3b06ab29011f026d5e1042594125e853032
novia <noviafan427@gmail.com>
2020-01-18 18:06:57 -0800

    idk why am i here

--- App.js
+++ App.js
@@ -1,23 +1,31 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import MatrixContainer from "./Components/MatrixContainer";
+import Header from './Components/Header.js';
+const headerTitle = 'To Do';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>I am trying to add more text to the file</Text>
-      <Text>I hate react native</Text>
-    </View>
-  );
+    return (
+      <View>
+      <StatusBar backgroundColor = 'black' barStyle="light-content"/>
+      <View style={styles.centered, styles.container}>
+        <Header title={headerTitle} />
+        <MatrixContainer />
+      </View>
+      </View>
+    );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    color: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000"
   },
-
+  centered: {
+    alignItems: 'center',
+  }
 });
+/*
+<Button title ="AHHHHHH" />
+      <TextInput placeholder="Write something here"/>
+*/
