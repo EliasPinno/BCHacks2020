cec14dc75849d579ba372443863d46b75fbd9608
Elias Pinno <pucas946@gmail.com>
2020-01-19 00:47:24 -0800

    Messing around with TaskBuilder display. Incomplete work

--- Components/TaskBuilder.js
+++ Components/TaskBuilder.js
@@ -1,28 +1,41 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { StyleSheet, Text, View, Button,TextInput,Slider } from 'react-native';
 
 const TaskBuilder = props => {
-  var state = {
-    slideValue: 0
-  }
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [tasks, setTasks] = useState([]);
+  var a = []
+  const titleInputHandler = (enteredText) => {
+    setEnteredTitle(enteredText);
+  };
+  const createTaskHandler = () =>{
+    //setTasks(tasks => [...tasks,enteredText])
+  };
+
   return(
     <View style ={styles.titleField}>
+
       <View style={styles.TextFields}>
         <Text>The title of the task</Text>
       </View>
+
       <View>
-        <TextInput placeholder="Write what the task is"/>
+        <TextInput placeholder="Write what the task is" onChangeText={titleInputHandler} value ={enteredTitle}/>
       </View>
+
       <View style={styles.TextFields}>
         <Text>How important is this task</Text>
       </View>
+
+      <View>
+        {tasks.map((task)=><Text>{task}</Text>)}
+      </View>
+
       <View style={styles.TextFields}>
         <Text>When does this task need to be done</Text>
       </View>
-      <View>
-      </View>
       <View style={styles.ButtonStyling}>
-        <Button title = "Create Task" color = "blue" />
+        <Button title = "Create Task" color = "blue" onPress={createTaskHandler}/>
       </View>
     </View>
   );
