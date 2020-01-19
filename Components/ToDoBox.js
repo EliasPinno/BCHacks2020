2ca8d88e0662ee6d3539757111a402f2fba8e575
Elias Pinno <pucas946@gmail.com>
2020-01-18 18:18:30 -0800

    Created Skeleton for the ToDoBox.

---
+++ Components/ToDoBox.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+const ToDoBox = props => {
+  return(
+    <View>
+
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 40,
+    backgroundColor: 'red'
+  },
+  textStyling: { 
+    color: 'black',
+    fontSize: 22,
+    fontWeight: '500'
+  }
+});
+export default ToDoBox;
