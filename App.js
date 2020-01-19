4b5fd72c2c2258d1d64933603303532f3c7ae61a
Elias Pinno <pucas946@gmail.com>
2020-01-18 17:39:53 -0800

    Making App Redner MatrixContainer
    Learned how to render other components, and this code succesfully makes the main app display a component

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput,Button } from 'react-native';
+import MatrixContainer from "./Components/MatrixContainer";
 
 export default function App() {
   return (
     <View style={styles.container}>
+    <MatrixContainer />
       <Text>Open up App.js to start working on your app!</Text>
       <Text>I am trying to add more text to the file</Text>
       <Text>I hate react native</Text>
@@ -19,5 +21,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-
 });
+/*
+
+<Button title ="AHHHHHH" />
+
+      <TextInput placeholder="Write something here"/>
+*/
